47c2c35890928490d15de7e9d393a68368ff0210
Alvaro Faundez <alvaro@faundez.net>
2018-04-03 19:18:30 -0400

    Add new build

--- docs/assets/script-main-85a74d152e14831052fa249204ca0d14fa8fe171fcd991b4b53d6c033288a3d8.js
+++ docs/assets/script-main-85a74d152e14831052fa249204ca0d14fa8fe171fcd991b4b53d6c033288a3d8.js
@@ -1,10 +1,14 @@
 (function() {
-  var elem, i, len, ref;
+  var elem, i, instance, len, ref;
 
   ref = document.querySelectorAll('.parallax');
   for (i = 0, len = ref.length; i < len; i++) {
     elem = ref[i];
     M.Parallax.init(elem);
   }
+
+  instance = M.Carousel.init({
+    fullWidth: true
+  });
 
 }).call(this);
